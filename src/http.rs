89cc026464d152322287bc306f88001ab7031460
kirkl4nd <77247639+kirkl4nd@users.noreply.github.com>
2024-06-13 21:17:13 -0400

    Moved html to separate file

--- src/http.rs
+++ src/http.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpServer, HttpResponse, HttpRequest, Responder};
+use std::fs;
 use std::path::{PathBuf, Path};
 use crate::fs::FileManager;
 
@@ -14,37 +15,36 @@ async fn file_or_directory_handler(req: HttpRequest, path: Option<web::Path<Stri
                 if file_type.is_dir() {
                     match file_manager.list_directory(&full_path) {
                         Ok(entries) => {
-                            // Start building the response
-                            let mut response = String::from("<html><head><title>");
-                            response.push_str(host);
-                            response.push_str("</title></head><body><h1>");
-                            response.push_str(host);
-                            response.push_str("</h1><ul>");
+                            let mut html_template = fs::read_to_string("src/web/index.html").unwrap_or_default();
 
-                            // Breadcrumb navigation
-                            let mut breadcrumb_path = String::new();
-                            response.push_str("<div><a href=\"/\">.</a> / ");
+                            // Replace placeholders
+                            html_template = html_template.replace("{{host}}", &host);
+
+                            let mut breadcrumb_navigation = String::from("<a href=\"/\">.</a> / ");
+                            let mut directory_contents = String::new();
                             if path_str != "." {
+                                let mut breadcrumb_path = String::new();
                                 for (index, component) in path_str.split('/').filter(|&c| !c.is_empty()).enumerate() {
                                     if index > 0 {
                                         breadcrumb_path.push('/');
                                     }
                                     breadcrumb_path.push_str(component);
                                     let link = format!(" <a href=\"/{0}\">{1}</a> / ", breadcrumb_path, component);
-                                    response.push_str(&link);
+                                    breadcrumb_navigation.push_str(&link);
                                 }
                             }
-                            response.push_str("</div>");
 
-                            // Directory contents
                             for entry in entries {
                                 let file_name = entry.file_name().unwrap().to_string_lossy();
                                 let link_path = format!("{}/{}", path_str, file_name);
                                 let link = format!("<li><a href=\"/{0}\">{1}</a></li>", link_path, file_name);
-                                response.push_str(&link);
+                                directory_contents.push_str(&link);
                             }
-                            response.push_str("</ul></body></html>");
-                            HttpResponse::Ok().content_type("text/html").body(response)
+
+                            html_template = html_template.replace("{{breadcrumb_navigation}}", &breadcrumb_navigation);
+                            html_template = html_template.replace("{{directory_contents}}", &directory_contents);
+
+                            HttpResponse::Ok().content_type("text/html").body(html_template)
                         },
                         Err(_) => HttpResponse::InternalServerError().finish(),
                     }
